3e60940b027f31bdb970798d8fe11c3816b75b7b
Sam Marxz <marxzsam@gmail.com>
2021-07-27 15:19:31 -0300

    feat: add globalStyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {GlobalStyles} from 'styles/global'
+
+function App({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare."
+        />
+        <title>Hapu - Create your nanny shares</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
-export default MyApp
+export default App
